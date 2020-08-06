0573ba779594cc4f162f9ba68202faa3e6e7d37d
Gabriel Asorey <gasorey@gmail.com>
2020-08-05 21:17:05 -0300

    feat: NLW day 2 completed

---
+++ server/src/routes.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import db from './database/connection';
+import convertHourToMinutes from './utils/convertHourToMinutes';
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
+
+const routes = express.Router()
+
+const classController = new ClassesController()
+const connectionsController = new ConnectionsController()
+
+
+
+routes.post('/classes', classController.create)
+routes.get('/classes', classController.index)
+
+routes.post('/connections', connectionsController.create)
+routes.get('/connections', connectionsController.index)
+
+ 
+
+export default routes
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express'
+import routes from './routes';
+import cors from 'cors'
+
 
 const app = express();
 
+app.use(cors())
+app.use(express.json())
 
-app.get('/', (request, response)=> {
-  return response.json({messa: 'hello world'})
-})
+app.use(routes)
 
 app.listen(3333)
